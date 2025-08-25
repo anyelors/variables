fffd59db76907d46dc0c6fd785c509fc35ba563d
anyelors <anyelors@gmail.com>
2025-08-25 17:09:28 +0200

    Clase Java Variables

---
+++ src/Constantes.java
@@ -0,0 +1,14 @@
+public class Constantes {
+    public static void main(String[] args) {
+        System.out.println("*** Constantes en Java ***");
+        final var DIAS_SEMANA = 7;
+        System.out.println("DIAS_SEMANA = " + DIAS_SEMANA);
+        final var PI = 3.1415;
+        System.out.println("PI = " + PI);
+        System.out.println("Math.PI = " + Math.PI);
+        final var MENSAJE_BIENVENIDA = "Bienvenido a Java";
+        System.out.println("MENSAJE_BIENVENIDA = " + MENSAJE_BIENVENIDA);
+
+        System.out.println("**************************");
+    }
+}
