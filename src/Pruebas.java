041efe80a9abc411a01ed5871411359d26f0e6e8
Anyelo Ramirez <anyelors@gmail.com>
2025-09-28 21:23:45 +0200

    Se actualiza archivo pruebas

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,14 +1,13 @@
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Pruebas {
 
     public static void main(String[] args) {
-        Map<Integer, List<String>> byLength =
-                List.of("java", "code", "stream", "tip", "web")
-                        .stream()
-                        .collect(Collectors.groupingBy(String::length));
+        Map<Integer, List<String>> byLength = List.of("java", "code", "stream", "tip", "web")
+                .stream()
+                .collect(Collectors.groupingBy(String::length));
 
         System.out.println("byLength = " + byLength);
 
@@ -24,6 +23,30 @@ public class Pruebas {
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
 
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(11);
+        list.add(34);
+        list.add(17);
+        list.add(20);
+
+        // 1.foreach(); method
+        Stream<Integer> myStream = list.stream();
+        myStream.forEach(x -> {
+            System.out.println(x);
+        });
+        System.out.println("********************************");
+
+        // 2. Stream Interface of(); Method - Creating Stream
+        Stream<String> myList = Stream.of("V", "D", "G", "R", "M");
+        myList.forEach(x -> {
+            System.out.println(x);
+        });
+        System.out.println("********************************");
+
+        // 2.filter(); Method - find odd number
+        list.stream().filter(x -> x % 2 != 0).forEach(System.out::println);
 
     }
+
 }
